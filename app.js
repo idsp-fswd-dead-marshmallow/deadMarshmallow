060a2cc670ba3bd39d0af4db7348027708d7f4b3
Kevin Hung <kevincjhung@gmail.com>
2022-03-26 23:16:19 -0700

    fixed bugs
    it now actually serves aboutUs.ejs

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const express = require("express");
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser");
 
-
-
 // use express
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static("public")); // access to public folder for all ejs files
+app.use(express.static("public")); // access to public folder for all ejs files
 app.set('view engine', 'ejs')
 
-// routes
+
+// routes || will probably need to set up routers at a later time
 app.get("/", (req, res) => {
-    res.render("aboutUs.html");
+    res.render("aboutUs");
 })
 
 
